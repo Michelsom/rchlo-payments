1010f72898530f35ac5209574b14837c689e9fc7
Michelsom <michelsonpagani@gmail.com>
2024-11-23 12:38:16 -0400

    feat:Refactor project configuration files and add support for SVG files

---
+++ src/assets/icons/creditCards/index.ts
@@ -0,0 +1,6 @@
+export {default as AmericanExpress} from './american-express.svg';
+export {default as Diners} from './diners.svg';
+export {default as Elo} from './elo.svg';
+export {default as Hipercard} from './hipercard.svg';
+export {default as MasterCard} from './master-card.svg';
+export {default as Visa} from './visa.svg';
